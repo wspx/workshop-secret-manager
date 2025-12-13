23ae422c5cdccd5e95ef062a8ea0212239edc602
William da Silva Pereira <william.wspx@gmail.com>
2025-12-13 19:39:00 -0300

    Startup

---
+++ src/test/java/br/com/qwasolucoes/sm/SmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.qwasolucoes.sm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
