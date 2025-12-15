54a0bb90fedb931b74af00fe2d739e3ef0083949
William da Silva Pereira <william.wspx@gmail.com>
2025-12-15 20:43:05 -0300

    Update SmApplication.java

--- src/main/java/br/com/qwasolucoes/sm/SmApplication.java
+++ src/main/java/br/com/qwasolucoes/sm/SmApplication.java
@@ -6,7 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SmApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
+		System.out.println("DB_URL_CONNECTION = " + System.getenv("DB_URL_CONNECTION"));
+		System.out.println("DB_DRIVER_CLASS = " + System.getenv("DB_DRIVER_CLASS"));
+		System.out.println("DB_USERNAME = " + System.getenv("DB_USERNAME"));
+		System.out.println("DB_PASSWORD = " + System.getenv("DB_PASSWORD"));
+		System.out.println("API_APININJA_URL = " + System.getenv("API_APININJA_URL"));
+		System.out.println("API_APININJA_TOKEN = " + System.getenv("API_APININJA_TOKEN"));
 		SpringApplication.run(SmApplication.class, args);
 	}
 
